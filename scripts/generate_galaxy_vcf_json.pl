5de51884685c44f385dc5387237d8ec7f3e1ea00
Scott Cain <scott+git@scottcain.net>
2020-03-30 06:20:57 +0000

    Major update: grouping SRA tracks by location, adding a hover balloon with useful info and coloring by frequency

--- scripts/generate_galaxy_vcf_json.pl
+++ scripts/generate_galaxy_vcf_json.pl
@@ -7,25 +7,35 @@ while (<DATA>) {
     chomp;
 
 
+    my ($id, $loc) = split /\t/;
+
     print
-    qq(       {
-         "category" : "Variants",
+    qq{       {
+         "category" : "Variants/$loc",
          "glyph" : "wormbase-glyphs/View/FeatureGlyph/Diamond",
-         "key" : "$_",
-         "label" : "$_",
+         "key" : "$id",
+         "label" : "$id",
          "maxHeight" : 3000,
          "metadata" : {
-            "about" : "Variation from the reference sequence for $_. For more information on the analysis that produced these data, see https://github.com/galaxyproject/SARS-CoV-2"
+            "about" : "Variation from the reference sequence for <a href='https://www.ncbi.nlm.nih.gov/sra/$id'>$id</a> from $loc. Green variants have a frequency of 5% or more; yellow, less than 5%. For more information on the analysis that produced these data, see https://github.com/galaxyproject/SARS-CoV-2"
          },
          "storeClass" : "JBrowse/Store/SeqFeature/VCFTabix",
          "style" : {
-            "height" : 8
+            "height" : 8,
+	    "color" : "function(f) {var af = f.get('AF'); af = af.values[0];  if (af < 0.05) {return 'gold';} return 'green';}"
+         },
+	 "onClick" : {
+            "label" : "function(track,f) {var af = f.get('AF'); af = af.values[0]; var start = f.get('start');var eff = f.get('EFF');eff = eff.values[0];var neweff = eff.replace('(', '</br>(');return '<b>Location:</b> '+start+'</br><b>Frequency:</b> '+af+'</br><b>Predicted effect:</b></br>'+neweff;}",
+            "url" : "",
+            "title" : "Variant data",
+            "action" : "defaultDialog"
          },
          "type" : "CanvasVariants",
          "unsafeMouseover" : true,
          "unsafePopup" : true,
-         "urlTemplate" : "https://s3.amazonaws.com/gmod-genomes-of-interest/jbrowse/data/SARS-CoV-2/VCF/$_.vcf.gz"
-      },)
+         "urlTemplate" : "https://s3.amazonaws.com/gmod-genomes-of-interest/jbrowse/data/SARS-CoV-2/VCF/$id.vcf.gz"
+      },
+      }
        , "\n";
 }
 
@@ -35,7 +45,7 @@ while (<DATA>) {
 
 
 
-#__DATA__
+__DATA__
 SRR10903401	Wuhan, China
 SRR11059946	Wuhan, China
 SRR11247075	Seattle, WA, USA
@@ -65,7 +75,7 @@ SRR11140748	Madison, WI, USA
 SRR11278090	Seattle, WA, USA
 SRR11393704	Unknown, USA
 SRR11397729	Melbourne, Australia
-SRR11410538     Salt Lake City, UT, USA
+SRR11410538	Salt Lake City, UT, USA
 SRR11410541	Salt Lake City, UT, USA
 SRR11059942	Salt Lake City, UT, USA
 SRR11140750	Madison, WI, USA
@@ -76,15 +86,15 @@ SRR11410542	Salt Lake City, UT, USA
 SRR11059943	Wuhan, China
 SRR11177792	Kathmandu, Nepal
 SRR11278092	Seattle, WA, USA
-SRR11397715	
-SRR11410528
-SRR11059944
-SRR11241254
-SRR11278164
-SRR11397716
-SRR11410529
-SRR11059945
-SRR11241255
-SRR11278165
-SRR11397717
-SRR11410532
+SRR11397715	Melbourne, Australia
+SRR11410528	Salt Lake City, UT, USA
+SRR11059944	Wuhan, China
+SRR11241254	Seattle, WA, USA
+SRR11278164	Seattle, WA, USA
+SRR11397716	Melbourne, Australia
+SRR11410529	Salt Lake City, UT, USA
+SRR11059945	Wuhan, China
+SRR11241255	Wuhan, China
+SRR11278165	Seattle, WA, USA
+SRR11397717	Melbourne, Australia
+SRR11410532	Salt Lake City, UT, USA
